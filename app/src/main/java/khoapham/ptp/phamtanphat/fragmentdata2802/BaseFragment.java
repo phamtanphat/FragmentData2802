1dad9cf669af32a7188603e5fbb0f890a0a05858
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-13 18:09:29 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentdata2802/BaseFragment.java
@@ -0,0 +1,24 @@
+package khoapham.ptp.phamtanphat.fragmentdata2802;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment{
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(setView(),container,false);
+        getViewinlayout(view);
+        return view;
+    }
+    public abstract int setView();
+    public abstract void getViewinlayout(View v);
+}
